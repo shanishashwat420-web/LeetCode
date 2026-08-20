935d2602cdf4176dbb1303d472ce60e55097d9ce
shanishashwat420-web <shanishashwat420@gmail.com>
2026-08-20 10:20:39 +0530

    Time: 28 ms (51.06%), Space: 45.5 MB (77.73%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,14 +1,36 @@
+// class Solution {
+//     public int triangleNumber(int[] nums) {
+//         int n =nums.length;
+//         int count =0;
+//         Arrays.sort(nums);
+//         for(int i =0;i<n;i++){
+//             for(int j =i+1;j<n;j++){
+//                 for(int k =j+1;k<n;k++){
+//                     if(nums[i]+nums[j]>nums[k]){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }https://leetcode.com/_next/static/images/dark-pending-f313d6fe32951fb6b4d48ad3ee4f3821.gif$0
+//         return count;
+//     }
+// }
+
 class Solution {
     public int triangleNumber(int[] nums) {
-        int n =nums.length;
-        int count =0;
         Arrays.sort(nums);
-        for(int i =0;i<n;i++){
-            for(int j =i+1;j<n;j++){
-                for(int k =j+1;k<n;k++){
-                    if(nums[i]+nums[j]>nums[k]){
-                        count++;
-                    }
+        int n = nums.length;
+        int count = 0;
+        for (int i = n - 1; i >= 2; i--) {
+            int j = 0;
+            int k = i - 1;
+
+            while (j < k) {
+                if (nums[j] + nums[k] > nums[i]) {
+                    count += k - j;
+                    k--;
+                } else {
+                    j++;
                 }
             }
         }
