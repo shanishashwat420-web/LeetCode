37a3c1501c4d0acd48835fc3a1833dc5252a3efc
shanishashwat420-web <shanishashwat420@gmail.com>
2026-09-01 08:56:33 +0530

    Time: 2 ms (99.34%), Space: 46.8 MB (85.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -16,22 +16,57 @@
 // }
 
 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+
+//         int n = nums.length;
+//         int left = 0;
+
+//         while (left < n - 1) {
+//             int right = left + 1;
+//             while (right < n) {
+//                 if (nums[left] + nums[right] == target) {
+//                     return new int[]{left, right};
+//                 }
+//                 right++;
+//             }
+//             left++;
+//         }
+//         return new int[]{};
+//     }
+// }
+
+// class Solution{
+//     public int[] twoSum(int[] nums, int target) {
+//         int n =nums.length;
+//     Arrays.sort(nums);
+//     int i =0;
+//     int j = n-1;
+//     while(i<j && i<n && j<n){
+//         int sum =nums[i]+nums[j];
+//         if(sum == target){
+//             return new int[]{i,j};
+//         }else if(sum<target){
+//             i++;
+//         }else{
+//             j--;
+//         }
+//     }
+//     return new int[]{};
+//     }
+// }
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        int n = nums.length;
-        int left = 0;
-
-        while (left < n - 1) {
-            int right = left + 1;
-            while (right < n) {
-                if (nums[left] + nums[right] == target) {
-                    return new int[]{left, right};
-                }
-                right++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remaing = target - nums[i];
+            if (map.containsKey(remaing)) {
+                return new int[]{map.get(remaing), i};
             }
-            left++;
+            map.put(nums[i], i);
         }
-        return new int[]{};
+        return new int[]{-1, -1};
     }
 }
