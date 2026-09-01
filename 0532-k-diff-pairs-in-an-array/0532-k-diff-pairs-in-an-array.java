129a4872c4dad8bff0b61020b75bf94ee09ea704
shanishashwat420-web <shanishashwat420@gmail.com>
2026-09-01 09:37:42 +0530

    Time: 10 ms (91.62%), Space: 46.6 MB (55.26%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -16,20 +16,72 @@
 // }
 
 
+// class Solution {
+//     public int findPairs(int[] nums, int k) {
+//         
+//         int n = nums.length;
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (i > 0 && nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+//             for (int j = i + 1; j < n; j++) {
+//                 if (Math.abs(nums[i] - nums[j]) == k) {
+//                     count++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+// wrong
+
+// class Solution{
+//      public int findPairs(int[] nums, int k){
+//         int n =nums.length;
+//         int left  =0;
+//         int right =nums.length-1;
+//         Arrays.sort(nums);
+//         int count =0;
+//         while(left<right){
+//             int diff =Math.abs(nums[left]-nums[right]);
+//             if(diff ==k){
+//                 count++;
+//             }else if(diff > k){
+//                 right--;
+//             }else{
+//                 left++;
+//             }
+//         }
+//         return count;
+//      }
+// }
+
 class Solution {
     public int findPairs(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length;
+        int left = 0;
+        int right = 1;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+        while (right < nums.length) {
+            if (left == right) {
+                right++;
             }
-            for (int j = i + 1; j < n; j++) {
-                if (Math.abs(nums[i] - nums[j]) == k) {
-                    count++;
-                    break;
-                }
+            else if (left > 0 && nums[left] == nums[left - 1]) {
+                left++;
+            }
+            else if (nums[right] - nums[left] < k) {
+                right++;
+            }
+            else if (nums[right] - nums[left] > k) {
+                left++;
+            }
+            else {
+                count++;
+                left++;
+                right++;
             }
         }
         return count;
